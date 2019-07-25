c781036b971251202617a9f49ee2bfce16ecb905
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-07-25 15:02:44 +0800

    准备支持搜索标识和标识转换器.

---
+++ src/main/java/com/jstarcraft/rns/search/annotation/SearchId.java
@@ -0,0 +1,18 @@
+package com.jstarcraft.rns.search.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 搜索标识
+ * 
+ * @author Birdy
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD })
+public @interface SearchId {
+
+}
---
+++ src/main/java/com/jstarcraft/rns/search/converter/IdConverter.java
@@ -0,0 +1,11 @@
+package com.jstarcraft.rns.search.converter;
+
+/**
+ * 标识转换器
+ * 
+ * @author Birdy
+ *
+ */
+public interface IdConverter {
+
+}
