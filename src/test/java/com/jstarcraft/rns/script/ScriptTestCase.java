3304ee0931233872ea4865bd15ecd89876499ec7
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-08-12 12:00:55 +0800

    使用JS脚本与JStarCraft框架交互.

--- src/test/java/com/jstarcraft/rns/script/ScriptTestCase.java
+++ src/test/java/com/jstarcraft/rns/script/ScriptTestCase.java
@@ -6,13 +6,13 @@ import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.jstarcraft.ai.evaluate.ranking.PrecisionEvaluator;
 import com.jstarcraft.ai.evaluate.ranking.RecallEvaluator;
 import com.jstarcraft.ai.evaluate.rating.MAEEvaluator;
 import com.jstarcraft.ai.evaluate.rating.MSEEvaluator;
+import com.jstarcraft.core.script.JsExpression;
 import com.jstarcraft.core.script.PythonExpression;
 import com.jstarcraft.core.script.ScriptContext;
 import com.jstarcraft.core.script.ScriptExpression;
@@ -23,17 +23,51 @@ import com.jstarcraft.rns.model.benchmark.RandomGuessModel;
 import com.jstarcraft.rns.task.RankingTask;
 import com.jstarcraft.rns.task.RatingTask;
 
-public class PythonTestCase {
+public class ScriptTestCase {
 
-    private static final ClassLoader loader = PythonTestCase.class.getClassLoader();
+    private static final ClassLoader loader = ScriptTestCase.class.getClassLoader();
 
+    /**
+     * 使用JS脚本与JStarCraft框架交互
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testJs() throws Exception {
+        // 获取JS脚本
+        File file = new File(ScriptTestCase.class.getResource("Model.js").toURI());
+        String script = FileUtils.readFileToString(file, StringUtility.CHARSET);
+
+        // 设置JS脚本使用到的Java类
+        ScriptContext context = new ScriptContext();
+        context.useClasses(Properties.class, Configurator.class);
+        context.useClasses(RankingTask.class, RatingTask.class, RandomGuessModel.class);
+        context.useClasses(Assert.class, PrecisionEvaluator.class, RecallEvaluator.class, MAEEvaluator.class, MSEEvaluator.class);
+        // 设置JS脚本使用到的Java变量
+        ScriptScope scope = new ScriptScope();
+        scope.createAttribute("loader", loader);
+
+        // 执行JS脚本
+        ScriptExpression expression = new JsExpression(context, scope, script);
+        Map<String, Float> data = expression.doWith(Map.class);
+        Assert.assertEquals(0.005825241F, data.get("precision"), 0F);
+        Assert.assertEquals(0.011579763F, data.get("recall"), 0F);
+        Assert.assertEquals(1.2708743F, data.get("mae"), 0F);
+        Assert.assertEquals(2.425075F, data.get("mse"), 0F);
+    }
+
+    /**
+     * 使用Python脚本与JStarCraft框架交互
+     * 
+     * @throws Exception
+     */
     @Test
     public void testPython() throws Exception {
         // 设置Python环境变量
         System.setProperty("python.console.encoding", StringUtility.CHARSET.name());
 
         // 获取Python脚本
-        File file = new File(PythonTestCase.class.getResource("Model.py").toURI());
+        File file = new File(ScriptTestCase.class.getResource("Model.py").toURI());
         String script = FileUtils.readFileToString(file, StringUtility.CHARSET);
 
         // 设置Python脚本使用到的Java类
@@ -44,7 +78,7 @@ public class PythonTestCase {
         // 设置Python脚本使用到的Java变量
         ScriptScope scope = new ScriptScope();
         scope.createAttribute("loader", loader);
-        
+
         // 执行Python脚本
         ScriptExpression expression = new PythonExpression(context, scope, script);
         Map<String, Double> data = expression.doWith(Map.class);
